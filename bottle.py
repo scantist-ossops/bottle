7e133ad97b8d3b1287e9a482048a08a7163331a4
Jackie <jwtan2010@gmail.com>
2023-11-29 17:04:19 +0800

    Applied patches to tag 0.10.12

--- bottle.py
+++ bottle.py
@@ -1921,7 +1921,7 @@ def parse_auth(header):
 
 def _parse_qsl(qs):
     r = []
-    for pair in qs.replace(';','&').split('&'):
+    for pair in qs.split('&'):
         if not pair: continue
         nv = pair.split('=', 1)
         if len(nv) != 2: nv.append('')
